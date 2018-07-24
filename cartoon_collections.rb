0a56b1741339daf1bad6feba2fd9ab545b445140
Forrest Benton <elliottbenton@gmail.com>
2018-07-24 01:13:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ end
 
 def summon_captain_planet(elements)
   elements.collect do |element|
-    element.
+    element.capitalize!
+    element << "!"
 end
 
 def long_planeteer_calls# code an argument here
